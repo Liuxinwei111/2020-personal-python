6c060b8fa9672eba9d7033c53da3719afce01301
aaa <842774212@qq.com>
2020-09-15 13:00:14 +0800

    lll

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,1 +1,10 @@
-print( "Hello World!" )
+import argparse
+import data
+
+def run():
+    my_parser = argparse.ArgumentParser(description='analysis the json file')
+    my_parser.add_argument('-i', '--init', help='json file path')
+    my_parser.add_argument('-u', '--user', help='username')
+    my_parser.add_argument('-r', '--repo', help='repository name')
+    my_parser.add_argument('-e', '--event', help='type of event')
+    args = my_parser.parse_args()
